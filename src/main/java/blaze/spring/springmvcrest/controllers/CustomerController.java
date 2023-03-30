a85d9efdec35d83fa74685f30be11b575aa881f4
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-03-30 19:55:07 +0200

    adding Post method in CustomerController and make some fixes

--- src/main/java/blaze/spring/springmvcrest/controllers/CustomerController.java
+++ src/main/java/blaze/spring/springmvcrest/controllers/CustomerController.java
@@ -2,9 +2,8 @@ package blaze.spring.springmvcrest.controllers;
 
 import blaze.spring.springmvcrest.domain.Customer;
 import blaze.spring.springmvcrest.services.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,16 @@ public class CustomerController {
     @GetMapping("")
     List<Customer> getAllCustomers() {
         return customerService.findAllCustomers();
+    }
+
+    @GetMapping("{id}")
+    public Customer getCustomerById(@PathVariable Long id) {
+        return customerService.findCustomerById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer saveCustomer(@RequestBody Customer customer) {
+        return customerService.saveCustomer(customer);
     }
 }
